4a93703a1b10dee95c20d5bd6665cafc46f41753
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-08-12 12:24:05 -0300

    Project Updating

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from recipes.views import home, contact
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('contact/', contact),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     # Put the specific path to avoid conflict. Ex: "recipes/home.html"
-    return render(request, 'recipes/home.html',)
+    return render(request, 'recipes/pages/home.html',)
     # The django search automatically the name path of "templates", because it's configurate in "settings.py" file, but you can configure it
-
-
-def contact(resquest):
-    return render(resquest, 'recipes/contact.html',)
