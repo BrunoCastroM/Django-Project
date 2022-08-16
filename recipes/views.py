3a9a052bb9a7fbd8a255bea90aaa7c84ac9e5c9c
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-08-16 11:40:29 -0300

    Recipe-view page

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,7 @@ def home(request):
     # Put the specific path to avoid conflict. Ex: "recipes/home.html"
     return render(request, 'recipes/pages/home.html',)
     # The django search automatically the name path of "templates", because it's configurate in "settings.py" file, but you can configure it
+
+
+def recipe(request,id):
+    return render(request, 'recipes/pages/recipe-view.html',)
