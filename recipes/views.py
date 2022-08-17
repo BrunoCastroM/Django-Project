f9fc028da7caf25a0957b150e75bc42cb0bf1b10
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-08-17 11:34:38 -0300

    Including random cards

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     # Put the specific path to avoid conflict. Ex: "recipes/home.html"
-    return render(request, 'recipes/pages/home.html',)
+    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(3)], })
     # The django search automatically the name path of "templates", because it's configurate in "settings.py" file, but you can configure it
 
 
-def recipe(request,id):
-    return render(request, 'recipes/pages/recipe-view.html',)
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe()})
