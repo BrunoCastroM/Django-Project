27df596d7e9813b391d018acb79bd1aa7f95a4d9
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-08-23 09:32:46 -0300

    Creating initial migration

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,15 @@
 from pydoc import describe
 from statistics import mode
+from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # In django you create models using classes
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -18,15 +24,5 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-
-# EDITED
-# title description slug
-# preparation_time preparation_time_unit
-# servings servings_unit
-# preparation_step
-# preparation_step_is_html
-# created_at updated_at
-# is_published
-# cover
-# category (Relação)
-# Author (Relação)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
