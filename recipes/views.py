6aec28d6bca387d8ecd890528953e94f60fc9689
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-08-29 10:43:57 -0300

    Adding authors in recipes according to the person's registration

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-
+from recipes.models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     # Put the specific path to avoid conflict. Ex: "recipes/home.html"
-    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(3)], })
+    return render(request, 'recipes/pages/home.html', context={'recipes': recipes, })
     # The django search automatically the name path of "templates", because it's configurate in "settings.py" file, but you can configure it
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={'recipes': recipes, })
 
 
 def recipe(request, id):
