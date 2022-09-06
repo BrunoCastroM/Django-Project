2d201bd28f879425f6a94fee6b751715458284c4
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-09-06 09:25:55 -0300

    Showing details page of recipe

--- recipes/views.py
+++ recipes/views.py
@@ -17,4 +17,5 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe(), 'is_detail_page': True})
+    recipe = Recipe.objects.filter(pk=id, is_published=True,).order_by('-id').first()
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': recipe, 'is_detail_page': True})
