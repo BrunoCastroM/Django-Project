49932b18229a5b5df9b85fabfebf10b61b694381
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-09-12 10:06:32 -0300

    Inicializing tests in project

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variable = '123456'
+        print('hello world')
+        assert 1 == 1, 'Um é igual a um'
