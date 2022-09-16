5cfe47f9c422d8f17b724a154474877254dc66d8
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-09-16 09:22:21 -0300

    Creating directory for tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -3,21 +3,6 @@ from django.urls import reverse, resolve
 from recipes import views
 
 
-# Testing url
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_detail_url_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
-
-
 # Testing view function
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_fuction_is_correct(self):
