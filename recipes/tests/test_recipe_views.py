781aec5c70a75d164984c18b27020ea4796c8a02
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-10-04 09:57:07 -0300

    Testing "is_published" in category and recipe

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -70,6 +70,15 @@ class RecipeViewsTest(RecipeTestBase):
         # Check with a one recipe exists
         self.assertIn(needed_title, content)
 
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False don't show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id})
+        )
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
@@ -91,3 +100,11 @@ class RecipeViewsTest(RecipeTestBase):
 
         # Check with a one recipe exists
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """Test recipe is_published False don't show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id}))
+        self.assertEqual(response.status_code, 404)
