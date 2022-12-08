7f208e5cbf050493f37ef3a3c1450ccc7519330a
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-12-08 10:37:18 -0300

    Reusing pagination with make_pagination

--- recipes/views.py
+++ recipes/views.py
@@ -1,41 +1,53 @@
-from django.http import Http404
-from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.db.models import Q
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.pagination import make_pagination
+
 from recipes.models import Recipe
-from django.core.paginator import Paginator
-from utils.pagination import make_pagination_range
 
 
 def home(request):
-    recipes = recipes = Recipe.objects.filter(is_published=True,).order_by('-id')
-    # Put the specific path to avoid conflict. Ex: "recipes/home.html"
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
 
-    try:
-        current_page = int(request.GET.get('page', 1))
-    except ValueError:
-        current_page = 1
-        
-    paginator = Paginator(recipes, 3)
-    page_obj = paginator.get_page(current_page)
+    page_obj, pagination_range = make_pagination(request, recipes, 3)
 
-    pagination_range = make_pagination_range(paginator.page_range, 4, current_page)
-
-    return render(request, 'recipes/pages/home.html', context={'recipes': page_obj, 'pagination_range': pagination_range})
-    # The django search automatically the name path of "templates", because it's configurate in "settings.py" file, but you can configure it
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': page_obj,
+        'pagination_range': pagination_range
+    })
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes, 'title': f'{recipes[0].category.name} - Category | '})
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
+    page_obj, pagination_range = make_pagination(request, recipes, 3)
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'title': f'{recipes[0].category.name} - Category | '
+    })
 
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
-    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': recipe, 'is_detail_page': True})
+
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
 
 
 def search(request):
     search_term = request.GET.get('q', '').strip()
+
     if not search_term:
         raise Http404()
 
@@ -43,11 +55,16 @@ def search(request):
         Q(
             Q(title__icontains=search_term) |
             Q(description__icontains=search_term),
-        ), is_published=True
+        ),
+        is_published=True
     ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
