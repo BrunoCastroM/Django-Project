754cc9d2d17d230bb9dd1e8c376e1c352a7013fa
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-12-26 10:56:58 -0300

    Configuring the forms with labels, widgets, errors, attrs, etc

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,27 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # exclude = ['fist_name']
+        labels = {
+            'username': 'Username',
+            'first_name': 'Fist name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The email must be valid'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty'
+            }
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your user'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
